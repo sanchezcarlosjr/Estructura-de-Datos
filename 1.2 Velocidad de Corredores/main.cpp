1441f89b7c7839644da9c6c7513bdac257bc96fc
CEST <boy_carlos_eduardo@hotmail.com>
2018-01-16 10:06:37 -0800

    Complete-Velocidad de Corredores

--- 1.2 Velocidad de Corredores/main.cpp
+++ 1.2 Velocidad de Corredores/main.cpp
@@ -42,12 +42,46 @@ class Runner
 };
 
 
-int main(int argc, char** argv) {
-	Runner runner(3,53);
-	cout<<"Velocidad es: "<< setprecision(2)  <<runner.calculateVelocity();
-	cout<<"\nEl tiempo del corredor es:";
-	cout<<" "<<runner.getMinutes()<<" Minutos";
-	cout<<" "<<runner.getSeconds()<<" Segundos";
+class ConsoleView
+{
+	public: void show()
+	{
+		int i=1;
+		int seconds, minutes;
+		bool flag=true;
+		do
+		{
+			cout<<"Tiempo del corredor "<<i++;
+			cout<<"\nMinutos: "; cin>>minutes;
+			cout<<"\nSegundos: "; cin>>seconds;
+			if( minutes == 0 && seconds == 0 )
+			{
+				flag = false;
+			}
+			else
+			{
+				this->showRunner(minutes, seconds);
+			}
+			cout<<"\n\n";
+		}while(flag);
+	}
 	
+	public: void showRunner(int minutes, int seconds)
+	{
+		Runner runner(minutes,seconds);
+		cout<<"Velocidad es: "<< setprecision(2)  <<runner.calculateVelocity();
+		cout<<"\nEl tiempo del corredor es:";
+		cout<<" "<<runner.getMinutes()<<" Minutos";
+		cout<<" "<<runner.getSeconds()<<" Segundos";
+	}
+	
+	
+	
+};
+
+
+int main(int argc, char** argv) {
+	ConsoleView console;
+	console.show();
 	return 0;
 }
