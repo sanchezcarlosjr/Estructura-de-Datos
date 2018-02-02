508486eead18f21edb8fd226152a8acb785d3ca7
CEST <boy_carlos_eduardo@hotmail.com>
2018-02-02 10:21:30 -0800

    Feat - Limit user input

--- Problema 3.4 Eliminar Clave/Text.cpp
+++ Problema 3.4 Eliminar Clave/Text.cpp
@@ -5,15 +5,37 @@ class Text
 {
 	string password, text;
 	
-	public: Text(string password, string text)
+	public: Text(string password, string temp)
 	{
 		this->password = password;
-		this->text = text;
+		this->text = delimitInput(temp);
 	}
+	
+	private: string delimitInput(string temp)
+	{
+		string limitedText;
+		int limitLineBreak = 10;
+		int lineBreak=0;
+		for(int i=0; i<temp.size(); i++)
+		{
+			if( temp[i] == '\n' )
+			{
+				lineBreak++;
+			}
+			if( lineBreak > limitLineBreak )
+			{
+				return limitedText;
+			}
+			limitedText += temp[i];
+		}
+		return limitedText;
+	}
+	
 	
 	public: string deletePassword()
 	{
-		return "Hola";
+		
+		return text;
 	}
 	
 	
