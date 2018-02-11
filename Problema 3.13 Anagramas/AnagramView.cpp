948a6cd5ee783130fcfdbfb08ae06d35109c5ee7
CEST <boy_carlos_eduardo@hotmail.com>
2018-02-10 18:02:16 -0800

    Initial Commit

---
+++ Problema 3.13 Anagramas/AnagramView.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+class AnagramView
+{
+	
+	public: interact()
+	{
+		cout<<"Ingrese la palabra 1: ";
+		cout<<"Ingrese la palabra 2:";	
+	}
+	
+	
+};
---
+++ Problema 3.13 Anagramas/AplicationView.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "AnagramView.cpp"
+#include "EscapesSequences.h"
+
+using std::string;
+using std::cout;
+
+
+class AplicationView
+{
+	string titleApp;
+	
+	public: AplicationView()
+	{
+		this->titleApp = "Problema 3.13 Anagramas"; 
+		this->titleApp += LINEBREAK;
+		this->titleApp += LINEBREAK;
+	}
+
+	public: start()
+	{
+		cout<<titleApp;
+		
+		AnagramView anagram;
+		anagram.interact();
+	}
+
+};
+
+
